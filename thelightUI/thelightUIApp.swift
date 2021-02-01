a5e3400cead6c3d7148cde31b5f03dd99bb16b4b
Andrey <andrey.dmr@gmail.com>
2021-02-01 20:20:30 +0300

    Initial Commit

---
+++ thelightUI/thelightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  thelightUIApp.swift
+//  thelightUI
+//
+//  Created by Andrey on 01.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct thelightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
